5b7b0f02787eaed2dc0bc423646832f8195867f7
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-06-11 22:27:21 -0700

    truthy vs faulty

--- solutions.js
+++ solutions.js
@@ -1,6 +1,18 @@
+//free code camp challenges that took me a while to finish, so I want to remember them
+
 //binary to readable code
 function binaryAgent(str) {
  return str.split(' ').map(e=> String.fromCharCode(parseInt(e,2))).join('')
 }
 
 binaryAgent(" 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
+// a code that check falty and truthy values in an object
+function truthCheck(collection, pre) {
+return collection.every((e,i)=>{
+  return (e[pre])
+})
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
